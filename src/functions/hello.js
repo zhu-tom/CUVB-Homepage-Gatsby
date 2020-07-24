deee1bbf501bac57e97a59fdbba94dbcae608c12
zhu-tom <zhutom01@gmail.com>
2020-07-24 03:54:04 -0400

    Update hello.js

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -1,14 +1,14 @@
 // For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
 const {MongoClient} = require('mongodb');
 
-
 export function handler(event, context, callback) {
-    console.log("queryStringParameters", event.queryStringParameters)
+    console.log("queryStringParameters", event.queryStringParameters);
+    const uri = `mongodb+srv://${process.env.GATSBY_DB_USER}:${process.env.GATSBY_DB_PASS}@${process.env.GATSBY_DB_URL}/${process.env.GATSBY_DB_NAME}?retryWrites=true&w=majority`;
     callback(null, {
       // return null to show no errors
       statusCode: 200, // http status code
       body: JSON.stringify({
-        msg: `Hello, World! ` + Math.round(Math.random() * 10),
+        msg: uri,
       }),
-    })
+    });
   }
