a4cd43af46dc958d959db5a8b7516f6a9a99203e
zhu-tom <zhutom01@gmail.com>
2020-07-24 03:20:58 -0400

    clean up

--- src/functions/mongodb-login.js
+++ src/functions/mongodb-login.js
@@ -4,14 +4,18 @@ export function handler(event, handler, callback) {
     const uri = `mongodb+srv://${process.env.GATSBY_DB_USER}:${process.env.GATSBY_DB_PASS}@${process.env.GATSBY_DB_URL}/${process.env.GATSBY_DB_NAME}?retryWrites=true&w=majority`;
     const client = new MongoClient(uri, {useNewUrlParser: true});
     client.connect().then(() => {
-        const db = client.db("database");
-        const users = db.collection("users");
-
-        users.findOne({email: 'admin'}).then((res) => {
-            callback(null, {
-                statusCode: 200,
-                body: JSON.stringify(res)
-            });
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({msg: "connected"})
         });
+        // const db = client.db("database");
+        // const users = db.collection("users");
+
+        // users.findOne({email: 'admin'}).then((res) => {
+        //     callback(null, {
+        //         statusCode: 200,
+        //         body: JSON.stringify(res)
+        //     });
+        // });
     });
 }
