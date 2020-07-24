57a0ae186593302a32e9ee6edbd232985ed8973e
zhu-tom <zhutom01@gmail.com>
2020-07-24 03:44:11 -0400

    updates

--- src/functions/mongodb-login.js
+++ src/functions/mongodb-login.js
@@ -3,23 +3,24 @@ const {MongoClient} = require('mongodb');
 export function handler(event, handler, callback) {
     const uri = `mongodb+srv://${process.env.GATSBY_DB_USER}:${process.env.GATSBY_DB_PASS}@${process.env.GATSBY_DB_URL}/${process.env.GATSBY_DB_NAME}?retryWrites=true&w=majority`;
     const client = new MongoClient(uri, {useNewUrlParser: true});
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({msg: "connected"})
-    });
-    // client.connect().then(() => {
-    //     callback(null, {
-    //         statusCode: 200,
-    //         body: JSON.stringify({msg: "connected"})
-    //     })
-    //     const db = client.db("database");
-    //     const users = db.collection("users");
+    client.connect().then((val) => {
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({msg: uri})
+        });
+        // const db = client.db("database");
+        // const users = db.collection("users");
 
-    //     users.findOne({email: 'admin'}).then((res) => {
-    //         callback(null, {
-    //             statusCode: 200,
-    //             body: JSON.stringify(res)
-    //         });
-    //     });
-    // });
+        // users.findOne({email: 'admin'}).then((res) => {
+        //     callback(null, {
+        //         statusCode: 200,
+        //         body: JSON.stringify(res)
+        //     });
+        // });
+    }).catch((err) => {
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(err)
+        });
+    });
 }
