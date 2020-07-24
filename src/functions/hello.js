bd5342ade3c72ad50e8e1ffd6bec15d5e61a0e25
zhu-tom <zhutom01@gmail.com>
2020-07-24 04:00:32 -0400

    test

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -2,13 +2,12 @@
 const {MongoClient} = require('mongodb');
 
 export function handler(event, context, callback) {
-    console.log("queryStringParameters", event.queryStringParameters);
-    const uri = `mongodb+srv://${process.env.GATSBY_DB_USER}:${process.env.GATSBY_DB_PASS}@${process.env.GATSBY_DB_URL}/${process.env.GATSBY_DB_NAME}?retryWrites=true&w=majority`;
+    console.log("queryStringParameters", event.queryStringParameters)
     callback(null, {
       // return null to show no errors
       statusCode: 200, // http status code
       body: JSON.stringify({
-        msg: uri,
+        msg: `Hello, World! ` + Math.round(Math.random() * 10),
       }),
-    });
+    })
   }
